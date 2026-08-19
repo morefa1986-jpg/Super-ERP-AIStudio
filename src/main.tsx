8f9d2ca9ef47575cdb600b84868c1b795c5f0ab4
morefa1986-jpg <morefa1986@gmail.com>
2026-08-19 14:29:21 +0330

    feat: launch ERP through integrated application shell

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
 import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
-import App from './App.tsx';
+import AppShell from './AppShell.tsx';
 import './index.css';
 import './ux-simplified.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <AppShell />
   </StrictMode>,
 );
